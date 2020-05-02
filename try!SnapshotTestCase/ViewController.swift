a4c1364a341fe4e448adfd2c43e8cb119d0b45cf
WeRockStar <kotchaphan.m@gmail.com>
2020-05-02 20:41:49 +0700

    Initial commit

---
+++ try!SnapshotTestCase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  try!SnapshotTestCase
+//
+//  Created by Kotchaphan Muangsan on 2/5/20.
+//  Copyright © 2020 Kotchaphan Muangsan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
